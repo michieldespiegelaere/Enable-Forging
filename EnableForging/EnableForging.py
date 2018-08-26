f898ac75ff0942b64513b2a441282f073ad2e483
michieldespiegelaere <michiel.despiegelaere@student.howest.be>
2018-08-26 19:15:50 +0200

    adding vars and putting the code in a function

--- EnableForging/EnableForging.py
+++ EnableForging/EnableForging.py
@@ -1,32 +1,38 @@
 import requests
 import time
-url = 'http://localhost:7000/api/node/status/forging' # api link
-json_data = requests.get(url).json()
-getData = json_data['data']
-forging =''
-headers = {'cache-control': 'no-cache', 'content-type': 'application/json'}
+ipAddress = ''
+port = ''
+
+
+def checkForging(ip, port):
+    url = 'http://' + ip + ':' + port + '/api/node/status/forging' # api link
+    json_data = requests.get(url).json()
+    getData = json_data['data']
+    headers = {'cache-control': 'no-cache', 'content-type': 'application/json'}
+    data = '{"publicKey": "","password": "","forging": true}'
 
 # gets the forging status out of the api call
-for i in getData:
-	forging = i['forging']
+    for i in getData:
+        forging = i['forging']
 
 # checks if forging is running
-if forging == True:
-	print('Your node is forging :)')
-
-else:
-    # if forging isn't running it should re enable it
-    print('Your node is not forging :\'(')
-    print('Enabling forging now')
-    
-    data = '{"publicKey": "","password": "","forging": true}'
-    response = requests.put('http://localhost:7000/api/node/status/forging', data=data, headers=headers)
-    time.sleep(5)
-    responseData = response.json()
-    getForgingResponseData = responseData['data']
-    for i in getForgingResponseData:
-        forging = i['forging']
     if forging == True:
-        print('Forging is enabled again')
+	    print('Your node is forging :)')
+
     else:
-        print('Something went wrong')
+# if forging isn't running it should re enable it
+        print('Your node is not forging :\'(')
+        print('Enabling forging now...')
+        response = requests.put(url, data=data, headers=headers)
+        time.sleep(2)
+        responseData = response.json()
+        getForgingResponseData = responseData['data']
+        for i in getForgingResponseData:
+            forging = i['forging']
+        if forging == True:
+            print('Forging is enabled again :D')
+        else:
+            print('Something went wrong :\'(')
+
+if __name__ == '__main__':
+    checkForging(ipAddress, port)
