d963c0e9fddc0dedffbc1146dac793d8b1a3efe1
michieldespiegelaere <michiel.despiegelaere@student.howest.be>
2018-08-27 10:28:40 +0200

    adding example ports in config file

--- EnableForging/Config.py
+++ EnableForging/Config.py
@@ -1,5 +1,5 @@
 ipAddress = ''  # Your nodes ipaddress
-port = ''       # The port you're using for your api 
+port = ''       # The port you're using for your api betanet :5000 testnet:7000 mainnet:8000
 publicKey = ''  # The public key of the forging delegate
 password = ''   # The password you used for encrypting your passphrase
 
